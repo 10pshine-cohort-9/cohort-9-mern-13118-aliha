2e7f52b1af8149ff92389c703cb6393a41fa471a
usertechbuddy <alihaasad19@gmail.com>
2026-08-13 13:29:33 +0500

    Troubleshooting the jest failure and addressing coderabbit issues

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'jsx'],
-  testMatch: ['**/tests/**/*.test.[jt]s?(x)'],
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  testMatch: ["**/tests/**/*.test.[jt]s?(x)"],
   transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
+    "^.+\\.[jt]sx?$": "babel-jest",
   },
   moduleNameMapper: {
-    '\\.(css|less|scss)$': '<rootDir>/tests/styleMock.cjs',
+    "\\.(css|less|scss)$": "<rootDir>/tests/styleMock.cjs",
   },
 };
